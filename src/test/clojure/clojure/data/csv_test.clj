8cceb687a7fad5b9e68d5ae4e2a57505388c8e53
Jonas Enlund <jonas.enlund@gmail.com>
2011-09-19 20:35:29 +0300

    added test for alternative separator

--- src/test/clojure/clojure/data/csv_test.clj
+++ src/test/clojure/clojure/data/csv_test.clj
@@ -6,13 +6,19 @@
    [java.io Reader StringReader StringWriter EOFException]))
 
 (def ^{:private true} simple
-     "Year,Make,Model
+  "Year,Make,Model
 1997,Ford,E350
 2000,Mercury,Cougar
 ")
 
+(def ^{:private true} simple-alt-sep
+  "Year;Make;Model
+1997;Ford;E350
+2000;Mercury;Cougar
+")
+
 (def ^{:private true} complicated
-     "1997,Ford,E350,\"ac, abs, moon\",3000.00
+  "1997,Ford,E350,\"ac, abs, moon\",3000.00
 1999,Chevy,\"Venture \"\"Extended Edition\"\"\",\"\",4900.00
 1999,Chevy,\"Venture \"\"Extended Edition, Very Large\"\"\",\"\",5000.00
 1996,Jeep,Grand Cherokee,\"MUST SELL!
@@ -20,6 +26,11 @@ air, moon roof, loaded\",4799.00")
 
 (deftest reading
   (let [csv (read-csv simple)]
+    (is (= (count csv) 3))
+    (is (= (count (first csv)) 3))
+    (is (= (first csv) ["Year" "Make" "Model"]))
+    (is (= (last csv) ["2000" "Mercury" "Cougar"])))
+  (let [csv (read-csv simple-alt-sep :separator \;)]
     (is (= (count csv) 3))
     (is (= (count (first csv)) 3))
     (is (= (first csv) ["Year" "Make" "Model"]))
