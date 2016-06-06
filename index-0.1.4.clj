4b7de6fd987e151f51fec64813d60c1f9f80f43c
Tom Faulhaber <git_com@infolace.com>
2016-06-06 12:42:18 -0700

    Autodoc commit for master/9b7934f8

--- index-0.1.4.clj
+++ index-0.1.4.clj
@@ -2,8 +2,7 @@
  ({:doc "Reading and writing comma separated values.",
    :author "Jonas Enlund",
    :name "clojure.data.csv",
-   :wiki-url
-   "http://clojure.github.com/data.csv/clojure.data.csv-api.html",
+   :wiki-url "http://clojure.github.io/data.csv/index.html",
    :source-url
    "https://github.com/clojure/data.csv/blob/2a4b8a365b1a3dabe4c9b5455c2b33523fcef559/src/main/clojure/clojure/data/csv.clj"}),
  :vars
@@ -20,7 +19,7 @@
    "Reads CSV-data from input (String or java.io.Reader) into a lazy\nsequence of vectors.\n\n Valid options are\n   :separator (default \\,)\n   :quote (default \\\")",
    :namespace "clojure.data.csv",
    :wiki-url
-   "http://clojure.github.com/data.csv//clojure.data.csv-api.html#clojure.data.csv/read-csv"}
+   "http://clojure.github.io/data.csv//index.html#clojure.data.csv/read-csv"}
   {:raw-source-url
    "https://github.com/clojure/data.csv/raw/2a4b8a365b1a3dabe4c9b5455c2b33523fcef559/src/main/clojure/clojure/data/csv.clj",
    :name "write-csv",
@@ -34,4 +33,4 @@
    "Writes data to writer in CSV-format.\n\nValid options are\n  :separator (Default \\,)\n  :quote (Default \\\")\n  :quote? (A predicate function which determines if a string should be quoted. Defaults to quoting only when necessary.)\n  :newline (:lf (default) or :cr+lf)",
    :namespace "clojure.data.csv",
    :wiki-url
-   "http://clojure.github.com/data.csv//clojure.data.csv-api.html#clojure.data.csv/write-csv"})}
+   "http://clojure.github.io/data.csv//index.html#clojure.data.csv/write-csv"})}
